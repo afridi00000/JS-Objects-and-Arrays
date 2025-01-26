bd76f6cd89279474db216c2cb1867e6f33c5d4c3
afridi00000 <afridi@usf.edu>
2025-01-25 19:36:47 -0500

    Update and rename cc_2.js to cc_2.js

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+// Task 1: Store Inventory
+let products = ["Apples", "Bananas", "Oranges", "Milk", "Bread"];
+products.push("Eggs"); // Add a new product
+products.pop(); // Remove the last product
+console.log("Task 1 - Updated Products:", products); 
