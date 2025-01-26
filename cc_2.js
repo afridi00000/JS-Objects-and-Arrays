e69aab024a9101336be1e2c707d23f9606c0345b
afridi00000 <afridi@usf.edu>
2025-01-25 19:37:01 -0500

    Update cc_2.js

--- cc_2.js
+++ cc_2.js
@@ -1,5 +1,1 @@
-// Task 1: Store Inventory
-let products = ["Apples", "Bananas", "Oranges", "Milk", "Bread"];
-products.push("Eggs"); // Add a new product
-products.pop(); // Remove the last product
-console.log("Task 1 - Updated Products:", products); 
+
